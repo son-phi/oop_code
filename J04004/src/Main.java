0363aac9270e2cc9a3224510bb2e2e7379c471fa
Phi Son <sonpq@mail.wgentech.com>
2024-10-04 13:25:49 +0700

    Update J04004 Fraction class

--- J04004/src/Main.java
+++ J04004/src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long num = sc.nextLong();
-        long den = sc.nextLong();
-        Fraction f1 = new Fraction(num, den);
-        f1.in();
+        long num1 = sc.nextLong();
+        long den1 = sc.nextLong();
+        Fraction f1 = new Fraction(num1, den1);
+        long num2 = sc.nextLong();
+        long den2 = sc.nextLong();
+        Fraction f2 = new Fraction(num2, den2);
+        Fraction res = f1.sum(f2);
+        res.in();
     }
 }
 
@@ -26,6 +27,13 @@ class Fraction {
     public static long gcd(long a, long b){
         if (b==0) return a;
         return gcd(b,a%b);
+    }
+    public Fraction sum(Fraction f2) {
+        long den = f2.denominator*this.denominator;
+        long num = this.numerator*f2.denominator + f2.numerator*this.denominator;
+
+        Fraction res = new Fraction(num, den);
+        return res;
     }
     public void in(){
         System.out.println(numerator+"/"+denominator);
