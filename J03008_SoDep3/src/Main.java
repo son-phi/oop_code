3747fdac0a59476a3248c5d64150ac3917ed3749
Phi Quoc Tu Son <phiquoctuson2004@gmail.com>
2024-10-06 16:25:29 +0700

    Add J03008_SoDep3

---
+++ J03008_SoDep3/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- > 0) {
+            String line = sc.nextLine();
+
+        }
+    }
+}
