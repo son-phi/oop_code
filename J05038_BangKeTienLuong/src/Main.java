03dc83bfc2a75dd614b66c082596a6a9972db2f5
Phi Quoc Tu Son <phiquoctuson2004@gmail.com>
2024-10-09 16:48:36 +0700

    Update J05038_BangKeTienLuong

--- J05038_BangKeTienLuong/src/Main.java
+++ J05038_BangKeTienLuong/src/Main.java
@@ -1,7 +1,87 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int soNV = sc.nextInt();
+        sc.nextLine();
+        ArrayList<NhanVien> arr = new ArrayList<>();
+        long tong = 0;
+        while(soNV-- > 0){
+            String hoTen = sc.nextLine();
+            long LCB = sc.nextLong();
+            byte soNgayCong = sc.nextByte();
+            sc.nextLine();
+            String chucVu = sc.nextLine();
+            NhanVien e = new NhanVien(hoTen,LCB, soNgayCong, chucVu);
+            tong += e.getThuNhap();
+            arr.add(e);
+            e.hienThiThongTin();
+        }
+        System.out.printf("Tong chi phi tien luong: %d",tong);
 
+    }
+}
+
+class NhanVien {
+    private String maNV;
+    private String hoTen;
+    private long luongCB;
+    private byte soNgayCong;
+    private String chucVu;
+    private long thuong;
+    private int phuCap;
+    private long thuNhap;
+    public static int stt = 0;
+    private long luongThang;
+    public NhanVien(String hoTen, long luongCB, byte soNgayCong, String chucVu) {
+        stt++;
+        this.maNV = String.format("NV%02d",stt);
+        this.hoTen = hoTen;
+        this.luongCB = luongCB;
+        this.chucVu = chucVu;
+        this.soNgayCong = soNgayCong;
+        this.thuong = tinhThuong();
+        this.phuCap = tinhPhuCap();
+        this.thuNhap = tinhThuNhap();
+        this.luongThang = (long)(luongCB)*(long)(soNgayCong);
+    }
+
+    private long tinhThuong(){
+        long luongThang = luongCB * soNgayCong;
+        if (soNgayCong >= 25){
+            return (long)  (luongThang*0.2);
+        } else if (soNgayCong >= 22){
+            return (long) (luongThang*0.1);
+        } else return 0;
+    }
+    private int tinhPhuCap(){
+        switch (chucVu){
+            case "GD":
+                return 250000;
+            case "PGD":
+                return 200000;
+            case "TP":
+                return 180000;
+            default:
+                return 150000;
+        }
+    }
+
+
+    private long tinhThuNhap(){
+        long luongThang = luongCB * soNgayCong;
+        return luongThang + thuong +phuCap;
+    }
+
+    public long getThuNhap() {
+        return thuNhap;
+    }
+
+    public void hienThiThongTin(){
+        System.out.printf("%s %s %d %d %d %d", maNV,hoTen,luongThang,thuong,phuCap,thuNhap);
+        System.out.println();
     }
 }
