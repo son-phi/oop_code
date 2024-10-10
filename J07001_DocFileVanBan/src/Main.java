ebc3637175750e885b25802cb99752015684da91
Phi Son <phiquoctuson2004@gmail.com>
2024-10-11 06:09:51 +0700

    Update J07001_DocFileVanBan

---
+++ J07001_DocFileVanBan/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("DATA.in")) ;
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
