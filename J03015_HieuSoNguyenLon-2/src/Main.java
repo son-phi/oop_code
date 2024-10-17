3763bad4170a2742af6de75cf80336c78fa5c5c7
Phi Son <phiquoctuson2004@gmail.com>
2024-10-17 14:14:19 +0700

    Update J03015_HieuSoNguyenLon-2

---
+++ J03015_HieuSoNguyenLon-2/src/Main.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger n = sc.nextBigInteger();
+        BigInteger m = sc.nextBigInteger();
+        BigInteger res = n.subtract(m);
+        System.out.println(res);
+    }
+}
